60f4c5601dceb5aca8ee7ff922ea367f9f967267
isaureduruy <58304623+isaureduruy@users.noreply.github.com>
2021-05-28 15:34:48 +0200

    Add files via upload

---
+++ ProjetPiscine/Espace Clients/inscription.php
@@ -0,0 +1,22 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inscription</title>
+</head>
+<body>
+
+	<form method="POST" action="" align="center">
+		
+		<input type="text" name="pseudo"> <br/>
+
+		<input type="password" name="mdp"> <br/>
+
+		<input type="submit" name="envoi">
+
+
+	</form>
+
+</body>
+</html>
